a6f3485d2134ec74e154e0a803e8dccb37161000
Rohit suman <rohit.sangod74@gmail.com>
2025-10-30 10:35:08 +0530

    chnage test route in index file

--- index.js
+++ index.js
@@ -50,7 +50,11 @@
     app.use("/api-employee", employRoute)
     // app.use("/apps", shopifyRoute);
 
+<<<<<<< Updated upstream
     app.get("/apps/agora-call", (req, res) => {
+=======
+    app.get("/apps/agora", (req, res) => {
+>>>>>>> Stashed changes
         // Shopify adds shop and other params automatically
         const shop = req.query.shop;
         res.redirect(`https://agora-ui-v2.netlify.app/home`);
