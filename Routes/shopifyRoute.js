81ebc862af891fe0810d0e69cbe5c7997871d4cc
Rohit suman <rohit.sangod74@gmail.com>
2025-12-03 13:34:21 +0530

    aa

--- Routes/shopifyRoute.js
+++ Routes/shopifyRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const shopifyRoute = express.Router();
-const { installShopifyApp, authCallback, shopifyLogin, proxyThemeAssetsController, proxyShopifyConsultantPage, proxyShopifyConsultantDashboard, proxyShopifyConsultantCards } = require('../Controller/shopifyController');
+const { installShopifyApp, authCallback, shopifyLogin, proxyThemeAssetsController, proxyShopifyConsultantPage, } = require('../Controller/shopifyController');
 const { deleteConsultant } = require('../Controller/consultantController');
 // Original route (Shopify automatically hit karta hai - MUST KEEP THIS!)
 shopifyRoute.get('/install', installShopifyApp);
@@ -8,8 +8,7 @@ shopifyRoute.get('/callback', authCallback);
 shopifyRoute.get('/login', shopifyLogin);
 shopifyRoute.get('/consultant-theme', proxyThemeAssetsController);
 shopifyRoute.get('/consultant-theme/login', proxyShopifyConsultantPage);
-shopifyRoute.get('/consultant-theme/dashboard', proxyShopifyConsultantDashboard);
-shopifyRoute.get('/consultant-theme/consultant-cards', proxyShopifyConsultantCards);
+
 
 
 module.exports = shopifyRoute;
