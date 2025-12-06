227e0ad10fb4c9fdb35254a20b35263124ecf95a
Rohit suman <rohit.sangod74@gmail.com>
2025-12-06 12:23:48 +0530

    a

--- MiddleWare/ShopifyMiddleware/helperTheme.js
+++ MiddleWare/ShopifyMiddleware/helperTheme.js
@@ -105,7 +105,7 @@ async function renderShopifyPage(req, res, iframeUrl, options = {}) {
       <!DOCTYPE html>
       <html>
         ${headHtml}
-        <body style="margin:0;padding:0;display:flex;flex-direction:column;min-height:90vh;">
+        <body style="margin:0;padding:0;display:flex;flex-direction:column;min-height:800px;">
           <header style="flex-shrink:0;">
             ${headerHtml}
           </header>
@@ -113,7 +113,7 @@ async function renderShopifyPage(req, res, iframeUrl, options = {}) {
             <iframe 
               id="agora-iframe"
               src="${iframeUrl}" 
-              style="border:none;width:100%;min-height:90vh;display:block;"
+              style="border:none;width:100%;min-height:800px;display:block;"
             ></iframe>
           </main>
           <footer style="flex-shrink:0;">
@@ -126,7 +126,7 @@ async function renderShopifyPage(req, res, iframeUrl, options = {}) {
               checkOrigin: false,
               autoResize: true,
               heightCalculationMethod: "bodyScroll",
-              minHeight: 90,
+              minHeight: 800,
             }, "#agora-iframe");
           </script>
         </body>
