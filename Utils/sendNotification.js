38e18fc7e70d0b0ecb9f84aed447d84336307666
Rohit suman <rohit.sangod74@gmail.com>
2025-12-09 18:19:43 +0530

    s

--- Utils/sendNotification.js
+++ Utils/sendNotification.js
@@ -3,14 +3,23 @@ const admin = require("firebase-admin");
 
 
 const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
     token_uri: process.env.FIREBASE_TOKEN_URI,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 };
+
+if (!serviceAccount) {
+    console.error("Firebase service account configuration is missing in environment variables.");
+    process.exit(1);
+}
 
 if (!admin.apps.length) {
     admin.initializeApp({
