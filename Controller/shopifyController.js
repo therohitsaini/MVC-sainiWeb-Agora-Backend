c0d3564cebd8d77d284f5f1f9e92a2a62288675e
Rohit suman <rohit.sangod74@gmail.com>
2025-12-19 10:13:38 +0530

    Update shopifyController.js

--- Controller/shopifyController.js
+++ Controller/shopifyController.js
@@ -19,7 +19,7 @@ try {
 }
 
 
-const frontendUrl = process.env.FRONTEND_URL || "https://rosa-max-become-asset.trycloudflare.com";
+const frontendUrl = process.env.FRONTEND_URL || "https://hygiene-varieties-estimation-learning.trycloudflare.com";
 
 const client_id = process.env.SHOPIFY_CLIENT_ID
 const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET
