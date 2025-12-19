d8d7b67b3d80ec9a18c4ce301c0b2a17db5b4a6e
Rohit suman <rohit.sangod74@gmail.com>
2025-12-19 15:55:52 +0530

    Update verifyWebHook.js

--- MiddleWare/ShopifyMiddleware/verifyWebHook.js
+++ MiddleWare/ShopifyMiddleware/verifyWebHook.js
@@ -1,11 +1,11 @@
-    const crypto = require("crypto");
-    const dotenv = require("dotenv");
-    dotenv.config();
+const crypto = require("crypto");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const verifyWebhook = (req) => {
     const hmac = req.headers["x-shopify-hmac-sha256"];
     const body = req.body;
-
+    console.log("body", req.headers, body);
     const hash = crypto
         .createHmac("sha256", process.env.SHOPIFY_API_SECRET)
         .update(body)
