0423cb14b5e29e43130a45358dd8a325f3c523d4
Rohit suman <rohit.sangod74@gmail.com>
2025-12-27 17:04:01 +0530

    Update videoCallController.js

--- Controller/videoCallController.js
+++ Controller/videoCallController.js
@@ -99,6 +99,7 @@ const generateVoiceToken = async (req, res) => {
 const getCaller_Receiver_Details = async (req, res) => {
    try {
       const { callerId, receiverId } = req.params;
+      console.log("req.params", req);
       console.log("callerId", callerId);
       console.log("receiverId", receiverId);
       if (!callerId || !receiverId) {
