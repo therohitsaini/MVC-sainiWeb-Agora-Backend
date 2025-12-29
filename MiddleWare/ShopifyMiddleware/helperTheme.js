ad70446d764302fa8ac5832d71db6fe4908218af
Rohit suman <rohit.sangod74@gmail.com>
2025-12-29 16:51:10 +0530

    Update helperTheme.js

--- MiddleWare/ShopifyMiddleware/helperTheme.js
+++ MiddleWare/ShopifyMiddleware/helperTheme.js
@@ -58,7 +58,7 @@ async function loadSections(shop, themeId, cookieHeader, userAgent, client) {
 /**
  * 🔥 MAIN REUSABLE FUNCTION
  */
-async function renderShopifyPage(req, res, allow = "", iframeUrl, options = {}) {
+async function renderShopifyPage(req, res, iframeUrl, options = {}) {
   const shop = req.query.shop;
   const themeId = req.query.theme_id;
   const cookieHeader = req.headers.cookie || "";
@@ -113,7 +113,7 @@ async function renderShopifyPage(req, res, allow = "", iframeUrl, options = {}) 
             <iframe 
               id="agora-iframe"
               src="${iframeUrl}" 
-               allow="${allow || ""}"
+               allow="microphone; camera"
               style="border:none;width:100%;min-height:90vh;display:block;"
             ></iframe>
           </main>
