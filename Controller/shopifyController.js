2849f66188f1780acbf57568868bb1bfc5e57eac
Rohit suman <rohit.sangod74@gmail.com>
2026-01-09 19:00:58 +0530

    Update shopifyController.js

--- Controller/shopifyController.js
+++ Controller/shopifyController.js
@@ -45,7 +45,7 @@ const appIsInstalled = async (req, res) => {
     const shop = req.params.shop;
     console.log("shop____appIsInstalled", shop);
     if (!shop) return res.status(400).send("Missing shop param");
-    const shopDoc = await shopModel.findOne({ shop });
+    const shopDoc = await shopModel.findOne({ shop: shop });
     console.log("shopDoc____appIsInstalled", shopDoc);
     if (shopDoc.accessToken) {
         return res.status(200).send(true);
