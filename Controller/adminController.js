0720488e6e7a48843632c1c937f128fa9cc8a34c
Rohit suman <rohit.sangod74@gmail.com>
2026-01-16 13:55:01 +0530

    Update adminController.js

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -175,6 +175,7 @@ const getTransactionController = async (req, res) => {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
         const skip = (page - 1) * limit;
+        console.log("_________________>", page, limit, skip,type);
 
         const filter = { shop_id: adminId };
         if (req.query.type) filter.type = req.query.type;
