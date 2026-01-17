2b48cc69d5ea44f27ed3585116375f5715bfbcab
Rohit suman <rohit.sangod74@gmail.com>
2026-01-17 11:02:09 +0530

    Update consultantRoute.js

--- Routes/consultantRoute.js
+++ Routes/consultantRoute.js
@@ -18,7 +18,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 
 consultantRoute.post("/add-consultant/:shop_id", upload.single("profileImage"), consultantController)
-consultantRoute.get("/api-find-consultant/:shop_id",verifyShopifyToken, getConsultant)
+consultantRoute.get("/api-find-consultant/:shop_id", getConsultant)
 consultantRoute.put("/api-consultant-update-status/:id", updateConsultantStatus)
 consultantRoute.get("/consultantid/:id", getConsultantById)
 consultantRoute.get("/consultant-history/:id", getConsultantHistory)
