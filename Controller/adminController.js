20abac4b7522b57a5a5bc3be3542413aa6e30155
Rohit suman <rohit.sangod74@gmail.com>
2026-01-17 12:14:05 +0530

    Update adminController.js

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -174,8 +174,11 @@ const getTransactionController = async (req, res) => {
         }
 
         const page = Number(req.query.page) || 1;
+        console.log("page", page);
         const limit = Number(req.query.limit) || 10;
+        console.log("limit", limit);
         const skip = (page - 1) * limit;
+        console.log("skip", skip);
         const type = req.query.type || "";
         console.log("_________________>", page, limit, skip, "______________", type);
         const typeValue = type === 0 ? 'all' : type === 1 ? 'chat' : type === 2 ? 'voice' : type === 3 ? 'video' : 'all';
