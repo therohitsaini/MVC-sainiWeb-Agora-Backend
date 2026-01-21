5536606006758643b740a304ecfa92b1ad5be742
Rohit suman <rohit.sangod74@gmail.com>
2026-01-21 17:41:39 +0530

    Update adminController.js

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -222,6 +222,8 @@ const getUserConsultantController = async (req, res) => {
         const { adminId } = req.params;
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
+        const search = req.query.search || "";
+        console.log("search??????????????????", search);
         console.log("page", page);
         console.log("limit", limit);
         const skip = (page - 1) * limit;
