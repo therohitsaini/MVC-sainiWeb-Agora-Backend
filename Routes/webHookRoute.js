1db204f171b9d7f7d674ac100d12c80ff0bac317
Rohit suman <rohit.sangod74@gmail.com>
2026-01-22 15:16:49 +0530

    Update webHookRoute.js

--- Routes/webHookRoute.js
+++ Routes/webHookRoute.js
@@ -9,9 +9,9 @@ const { verifyWebhook } = require('../MiddleWare/ShopifyMiddleware/verifyWebHook
 webHookRoute.post('/webhooks/orders-created', express.raw({ type: "application/json" }), verifyWebhook, webhooksOrdersCreated);
 webHookRoute.post('/webhooks/orders-deleted', express.raw({ type: "application/json" }), verifyWebhook, webhooksOrdersDeleted);
 webHookRoute.post('/app-uninstalled', express.raw({ type: "application/json" }), verifyWebhook, webhooksAppUninstalled);
-webHookRoute.post('/customers/data_request', express.raw({ type: "application/json" }), verifyWebhook, webhooksCustomerDataRequest);
-webHookRoute.post('/customers/redact', express.raw({ type: "application/json" }), verifyWebhook, webhooksCustomerRedact);
-webHookRoute.post('/shop/redact', express.raw({ type: "application/json" }), verifyWebhook, webhooksShopRedact);
+webHookRoute.post('/customers/data_request', verifyWebhook, webhooksCustomerDataRequest);
+webHookRoute.post('/customers/redact', verifyWebhook, webhooksCustomerRedact);
+webHookRoute.post('/shop/redact', verifyWebhook, webhooksShopRedact);
 
 
 module.exports = { webHookRoute };
