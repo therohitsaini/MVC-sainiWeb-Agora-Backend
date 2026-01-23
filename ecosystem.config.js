660014ee17751905e9bd280247686c304bc3639c
Rohit suman <rohit.sangod74@gmail.com>
2026-01-23 18:20:40 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,8 @@ module.exports = {
     apps: [
         {
             name: "test-online-consultation",
-            script: "index.js",   // ya app.js
-            watch: true,
-            ignore_watch: [
-                "node_modules",
-                "uploads",
-                "uploads/consultants",
-                ".git",
-                "logs",
-                "*.log"
-            ],
+            script: "index.js", // ya app.js
+            watch: false,       // 🔴 MOST IMPORTANT
             env: {
                 NODE_ENV: "production"
             }
