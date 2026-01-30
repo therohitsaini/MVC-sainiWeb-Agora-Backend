f6575443929ca2faf4e08df66a08440081ba5f76
Rohit suman <rohit.sangod74@gmail.com>
2026-01-30 11:21:58 +0530

    Update adminPrincingController.js

--- Controller/adminPrincingController.js
+++ Controller/adminPrincingController.js
@@ -19,7 +19,7 @@ const pricingCallback = async (req, res) => {
 
         shop_.planStatus = "ACTIVE";
         await shop_.save();
-        const breack_domain = shop.shop
+        const breack_domain = shop_.shop[1]
         console.log("breack_domain", breack_domain)
 
         return res.send(`
