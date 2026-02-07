514c8a2028b21f29384ce25269eef06d47559ce0
Rohit suman <rohit.sangod74@gmail.com>
2026-02-07 15:55:54 +0530

    fixed

--- Helper/helper.js
+++ Helper/helper.js
@@ -1,6 +1,8 @@
-export const formatTime = (totalSeconds) => {
+const formatTime = (totalSeconds) => {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
 
     return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 };
+
+module.exports = { formatTime }
