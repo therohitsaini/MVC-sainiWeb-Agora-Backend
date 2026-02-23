e46802ae14bf751df2999002b41e4aecf53e82d1
Rohit suman <rohit.sangod74@gmail.com>
2026-02-23 12:47:26 +0530

    a

--- Controller/adminPrincingController.js
+++ Controller/adminPrincingController.js
@@ -1,4 +1,18 @@
 const { shopModel } = require("../Modal/shopify");
+const axios = require("axios");
+
+async function getChargeDetails(shop, accessToken, charge_id) {
+    const url = `https://${shop}/admin/api/2023-10/recurring_application_charges/${charge_id}.json`;
+
+    const response = await axios.get(url, {
+        headers: {
+            "X-Shopify-Access-Token": accessToken,
+            "Content-Type": "application/json"
+        }
+    });
+
+    return response.data.recurring_application_charge;
+}
 
 const pricingCallback = async (req, res) => {
     try {
@@ -15,6 +29,12 @@ const pricingCallback = async (req, res) => {
         const shop_ = await shopModel.findOne({ shop });
 
         if (!shop_) return res.status(404).send("Shop not found");
+        const charge = await getChargeDetails(
+            shop,
+            shop_.accessToken,
+            charge_id
+        );
+        console.log("charge", charge)
         let planName;
         if (charge_id === "26034864191") {
             planName = "Basic"
@@ -29,7 +49,7 @@ const pricingCallback = async (req, res) => {
                 `);
 
 
-    } catch (err) { 
+    } catch (err) {
         console.error(err);
         res.status(500).send("Billing callback failed");
     }
