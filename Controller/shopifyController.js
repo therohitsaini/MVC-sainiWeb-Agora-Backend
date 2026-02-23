022f37cdfc1eef4080e027286694f091918c6cc3
Rohit suman <rohit.sangod74@gmail.com>
2026-02-23 10:30:25 +0530

    aa

--- Controller/shopifyController.js
+++ Controller/shopifyController.js
@@ -643,6 +643,8 @@ const proxyProfileSection = (req, res) => {
     console.log("___________customerId___________", customerId);
     console.log("shopId", shopId);
     console.log("consultantId", consultantId);
+    console.log("shop", shop);
+
     if (!customerId) {
         return res.redirect(`https://${shop}/account/login`);
     }
