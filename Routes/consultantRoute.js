9cb037fe631034a75f279cb6269b1bb1a9eee52a
Rohit suman <rohit.sangod74@gmail.com>
2026-02-28 11:53:11 +0530

    a

--- Routes/consultantRoute.js
+++ Routes/consultantRoute.js
@@ -37,7 +37,7 @@ consultantRoute.get("/consultant-history/:id", getConsultantHistory)
 consultantRoute.get("/consultant-all-user/:id", getConsultantAllUser)
 consultantRoute.get("/consultant-all-user-history", getConsultantAllUserHistory)
 consultantRoute.delete('/delete-consultant/:id', deleteConsultant);
-consultantRoute.get("/consultant-by-shop-id-and-consultant-id/:shop_id/:consultant_id", authenticateToken, getConsultantByShopIdAndConsultantId);
+consultantRoute.get("/consultant-by-shop-id-and-consultant-id/:shop_id/:consultant_id", getConsultantByShopIdAndConsultantId);
 consultantRoute.post("/login-consultant", loginConsultant);
 consultantRoute.get("/get/chat-list/:shop_id/:consultant_id", authenticateToken, getChatListByShopIdAndConsultantId);
 consultantRoute.delete("/remove/user/chat-list/:id/:senderId", removeChatListAndConsultantIdFromChatList);
